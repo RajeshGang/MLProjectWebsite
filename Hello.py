f08cefcf9df387c67d609cb78d54e99c78dd70e1
RajeshGang <80492203+RajeshGang@users.noreply.github.com>
2024-10-04 22:45:11 -0400

    Final changes for the website

--- Hello.py
+++ Hello.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "Hello"
+    page_title = "Home"
 )
-st.title("Hi")
-st.write("""xxyxyxyxyxyxyx""")
+st.title("NFL Forecasting Game Outcome Predictor")
+st.write("Group 10: Rahul Rajesh, Vishnu Varma, Ishaan Goswami, Rohan Mathew, Sattwik Malawaram")
+st.write("Here is the link to our github repository: https://github.com/rohznmdev/ML-Football-Prediction-Model")
+st.write("Here is the link to our video: https://youtu.be/8GQPK6ewCWQ")
